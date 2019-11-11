fc48cbaecc281310e60a989dbdacac8edef549cf
Toosdai Otte <toosdai52Otte@gmail.com>
2019-11-10 19:15:24 -0600

    initial setup complete, logger method working

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const express = 'express';
+const express = require('express');
+// const helmet = require('helmet');
+// const morgan = require('morgan');
+const userRouter = require('./users/userRouter.js');
 
 const server = express();
+
+server.use(express.json());
+
+server.use('/user', userRouter);
+// server.use(helmet());
+// server.use(morgan('dev'));
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +19,8 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`${req.method} Request, ${req.url}`, Date.now());
+  next();
 };
 
 module.exports = server;
